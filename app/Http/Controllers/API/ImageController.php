9cdd46a51159e0501422a8f772b82c39203d26ed
kaxterzz <kasunsmbox@hotmail.com>
2019-06-26 16:35:38 +0530

    added image upload function successfully

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -5,7 +5,6 @@ use File;
 use Image;
 use App\Content;
 use App\User;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -31,19 +30,18 @@ class ImageController extends Controller
     {
 
         $file = $request->file;
-        $fname = $file['name'];
-        $f = finfo_open();
         $image_decode = base64_decode($file);
         $image_data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $file));
-        $mime_type = finfo_buffer($f, $image_data, FILEINFO_MIME_TYPE);
-        //$imageName = "image-".time().'.'.str_replace("image/","",$mime_type);
-        
-        //$png_url = "product-".time().".png";
-        $path = public_path().'/uploads/api-images/' . $fname;
 
-        Image::make(file_get_contents($request->file))->save($path);     
-       
-        $path = "uploads/api-images/".$fname;
+        $f = finfo_open();
+        $mime_type = finfo_buffer($f, $image_data, FILEINFO_MIME_TYPE);
+        $imageName = "image-".time().'.'.str_replace("image/","",$mime_type);
+
+        $tmpFilePath=sys_get_temp_dir().'/'.uniqid();
+        file_put_contents($tmpFilePath, $image_data);
+        $tmpFile=new File($tmpFilePath);
+        File::move($tmpFilePath, storage_path("app/public/api-images/$imageName"));
+        $path = "uploads/api-images/".$imageName;
 
         $max_id = User::max('id');
         $content = new Content;
