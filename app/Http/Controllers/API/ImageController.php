9856b01368ad75ee2552f56b5e6c2cb99c45423b
kaxterzz <kasunsmbox@hotmail.com>
2019-07-08 15:42:11 +0530

    werwer

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -39,7 +39,7 @@ class ImageController extends Controller
             //$content = $request->all();
             //$base = $_POST["file"];
             $bodyContent = $request->getContent();
-            echo $bodyContent['file'];
+            echo $bodyContent->file;
             //dd($content);
             //dd($content['file']);
 
