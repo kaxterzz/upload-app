a143463100451f124b8efec817a3a3372c7ffee4
kaxterzz <kasunsmbox@hotmail.com>
2019-07-08 14:46:11 +0530

    ewewew

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -34,6 +34,7 @@ class ImageController extends Controller
         try {
             //header('Content-Type: bitmap; charset=utf-8');
             $img = $request->file;
+            echo $img;
             $username = $request->Username;
            // $image_decode = base64_decode($file);
             // $image_data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $file));
